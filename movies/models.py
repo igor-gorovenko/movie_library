f17b841caea9613a6b94d6dabf2b31163fff751c
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-05-28 11:34:29 +0300

    add detail pages, create templates, update views and models

--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import date
 
+from django.urls import reverse
+
 
 class Category(models.Model):
     """Категории"""
@@ -79,6 +81,11 @@ class Movie(models.Model):
     )
     url = models.SlugField(max_length=160, unique=True)
     draft = models.BooleanField("Черновик", default=False)
+
+
+    def get_absolute_url(self):
+        return reverse('movie_detail', kwargs={'slug': self.url})
+
 
     def __str__(self):
         return self.title
