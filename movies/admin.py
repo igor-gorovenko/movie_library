a68470a0828b49fb746e915ea0decce42e07615b
igorforspeak@gmail.com <igorforspeak@gmail.com>
2023-05-31 19:10:49 +0300

    добавил экшены в админ панель

--- movies/admin.py
+++ movies/admin.py
@@ -50,6 +50,9 @@ class MovieAdmin(admin.ModelAdmin):
     save_on_top = True
     save_as = True
     list_editable = ('draft',)
+
+    actions = ["publish", "unpublish"]
+    
     form = MovieAdminForm
     readonly_fields = ('get_image', )
     fieldsets = (
@@ -77,6 +80,34 @@ class MovieAdmin(admin.ModelAdmin):
     def get_image(self, obj):
         return mark_safe(f'<img src={obj.poster.url} width="100" height="120"')
     
+
+    def unpublish(self, request, queryset):
+        """Снять с публикации"""
+        row_update = queryset.update(draft=True)
+        if row_update == 1:
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f"{row_update} запись была обновлена"
+        self.message_user(request, f"{message_bit}")
+
+    def publish(self, request, queryset):
+        """Опубликовать"""
+        row_update = queryset.update(draft=False)
+        if row_update == 1:
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f"{row_update} запись была обновлена"
+        self.message_user(request, f"{message_bit}")
+
+
+    publish.short_description = 'Опубликовать'
+    publish.allowed_permissions = ('change', )
+
+
+    unpublish.short_description = 'Снять с публикации'
+    unpublish.allowed_permissions = ('change', )
+
+
     get_image.short_description = 'Постер'
 
 
